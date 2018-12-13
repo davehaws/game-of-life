50b8e836b7bb8407af801c124ffb29df00db0c3a
Dave Haws <davehaws@users.noreply.github.com>
2018-12-13 13:53:58 -0600

    Pulled out code that was not being used since I moved the evolution code to the board. I think that was a mistake... Maybe should move evolution back to game, and create a Cell Iterator for the board.

--- src/GameOfLife/Game.java
+++ src/GameOfLife/Game.java
@@ -1,8 +1,6 @@
 package GameOfLife;
 
 
-import static GameOfLife.Cell.State.*;
-
 public class Game {
 
 	private int dimension = 10;
@@ -14,67 +12,6 @@ public class Game {
 
 	public Board getBoard() {
 		return board;
-	}
-
-	public boolean cellIsAlive(int x, int y) {
-		if (coordinatesAreIllegal(x, y)) {
-			throw new IndexOutOfBoundsException();
-		}
-			
-		return board.getCell(new Location(x,y)).is(ALIVE);
-	}
-
-	public void tick() {
-		Board newBoard = new Board(dimension);
-		for (int x = 0; x < dimension; x++) {
-			for (int y = 0; y < dimension; y++) {
-				int neighborCount = getNeighborCount(x, y);
-				Location loc = new Location(x, y);
-				Cell newCell = board.getCell(loc).nextGen(neighborCount);
-				if (newCell.is(ALIVE)) {
-					newBoard.getCell(loc).setState(ALIVE);
-				}
-			}
-		}
-		board = newBoard;
-	}
-
-	public void setDimension(int newDimension) {
-		dimension = newDimension;
-		board = new Board(dimension);
-	}
-
-	private int getCellState(int x, int y) {
-		if (coordinatesAreIllegal(x, y)) {
-			return 0;
-		}
-		
-		Location loc = new Location(x, y);
-		return board.getCell(loc).is(ALIVE) ? 1 : 0;
-	}
-
-	private boolean coordinatesAreIllegal(int x, int y) {
-		if (x < 0 || y < 0) {
-			return true;
-		}
-		if (x >= dimension || y >= dimension) {
-			return true;
-		}
-		return false;
-	}
-	
-	public int getNeighborCount(int x, int y) {
-		int count = 0;
-		count += getCellState(x-1, y-1);
-		count += getCellState(x-1, y);
-		count += getCellState(x-1, y+1);
-		count += getCellState(x, y-1);
-		count += getCellState(x, y+1);
-		count += getCellState(x+1, y-1);
-		count += getCellState(x+1, y);
-		count += getCellState(x+1, y+1);
-
-		return count;
 	}
 
 }
--- test/GameOfLife/GameTest.java
+++ test/GameOfLife/GameTest.java
@@ -8,15 +8,6 @@ public class GameTest {
 	private Game game = new Game();
 	
 	@Test
-	public void game_has_a_default_dimension_with_blank_board() {
-		game.setDimension(10);
-		assertThat(game.getDimension(), is(10));
-		assertThat(game.cellIsAlive(0,0), is(false));
-		assertThat(game.cellIsAlive(9,9), is(false));
-		assertThat(game.cellIsAlive(3,7), is(false));
-	}
-	
-	@Test
 	public void game_has_access_to_board() {
 		assertThat(game.getBoard(), is(notNullValue()));
 	}
